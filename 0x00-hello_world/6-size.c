d15365aecab03e8a106044b11f270dd77dec49fa
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 18:24:06 -0700

    yeh

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdint.h>
 /**
  * main - this program prints various sizes of data types
  * Return: its return value is 0
@@ -12,10 +13,15 @@ int main(void)
 	long long int d;
 	float e;
 
-	printf("size of a char: %zu", sizeof(a));
-	printf("size of an int: %zu", sizeof(b));
-	printf("size of a long int: %zu", sizeof(c));
-	printf("size of a long long int: %zu", sizeof(c));
-	printf("size of a float: %zu", sizeof(d));
+printf("size of a char: %u", (uint32_t)sizeof(a));
+printf("size of an int: %u", (uint32_t)sizeof(b));
+printf("size of a long int: %u", (uint32_t)sizeof(c));
+printf("size of a long long int: %u", (uint32_t)sizeof(c));
+printf("size of a float: %u", (uint32_t)sizeof(d));
+printf("size of a char: %u", (uint64_t)sizeof(a));
+printf("size of an int: %u", (uint64_t)sizeof(b));
+printf("size of a long int: %u", (uint64_t)sizeof(c));
+printf("size of a long long int: %u", (uint64_t)sizeof(c));
+printf("size of a float: %u", (uint64_t)sizeof(d));
 	return (0);
 }
