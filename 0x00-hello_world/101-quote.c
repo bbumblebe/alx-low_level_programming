9490033d07ef77ad05ddd6a025c07dc4a556a285
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 19:22:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,7 +6,12 @@
 int main(void)
 {
 	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int i = 0;
 
-	putchar(a);
+	while (a[i] != '\0')
+	{
+	putchar(a[i]);
+	i++;
+	}
 	return (0);
 }
