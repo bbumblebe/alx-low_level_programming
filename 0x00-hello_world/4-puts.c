a9bd21d4b1f247e7f06a56b43b5ffda0bdc0ebfe
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 17:27:41 -0700

    main - a c program that prints with puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include<stdio.h>
 /**
- * main - a c program that prints with puts
- * main - a c program that prints with puts
- * Return - this the return value 0
+ * main - A program that prints a line with puts function
+ * Return: this the return value 0
  */
 int main(void)
 {
