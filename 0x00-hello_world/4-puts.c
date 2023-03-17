ad4c47e873f476160916b11adf28abdc0d9b61bf
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 17:22:02 -0700

    main - a c program that prints with puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - a c program that prints with puts
+ * main - a c program that prints with puts
+ * Return - this the return value 0
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
