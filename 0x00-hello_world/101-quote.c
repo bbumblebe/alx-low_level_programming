0eb9588b4da3c40f8bba42fe53fede8a6d83394e
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 19:12:48 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+/**
+ * main - our program will use a string
+ * Return: the return value is 0
+ */
+int main(void)
+{
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	putchar(a);
+	return (0);
+}
