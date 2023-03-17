95d22924f974535975e7fab85eebd9237121c2f6
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 17:34:57 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+/**
+ * main - a program that uses print f
+ * Return: this will return 0
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
