365e932ba0bfc94e26c98352214c0ab3e473d8f5
bbumblebe <musweugeoffrey@gmail.com>
2023-03-16 19:24:21 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  */
 int main(void)
 {
-	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char a[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 	int i = 0;
 
 	while (a[i] != '\0')
