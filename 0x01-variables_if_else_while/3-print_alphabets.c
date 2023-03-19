09eb4f46f9e6a80fa46442dcc8f5add741d846a2
bbumblebe <musweugeoffrey@gmail.com>
2023-03-19 12:10:38 -0700

    3-print_alphabets.c

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - this program prints letters minus q and e
+ * Return: the return value is 0
+ */
+
+int main(void)
+{
+	char a[] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
