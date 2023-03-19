aeca8b6c30273a812aeafb908125e0cffa984e6f
bbumblebe <musweugeoffrey@gmail.com>
2023-03-19 06:28:19 -0700

    0-positive_or_negative.c

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,7 +2,7 @@
 #include <time.h>
 /**
  * main - this code helps us determine the sign of n
- * return: the return value is 0
+ * Return: Returns value is 0
  */
 
 int main(void)
@@ -12,16 +12,14 @@ int main(void)
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
-	for (n == n)
-	{
-		if (n > 0);
-		printf("%d is positive", n);
+		if (n > 0)
+		printf("%d is positive\n", n);
 
-		else if (n = 0);
-		printf("%d is zero", n);
+		else if (n == 0)
+		printf("%d is zero\n", n);
 
-		else (n <0);
-		printf("%d is negative", n);
-	}
-	return(0);
+		else
+		printf("%d is negative\n", n);
+
+	return (0);
 }
