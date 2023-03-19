f6b9a8623f25f8c5e8c4364107816deea4da8337
bbumblebe <musweugeoffrey@gmail.com>
2023-03-19 16:30:18 -0700

    223

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+/**
+ * main - program that prints all possible combinatioms
+ * Return: the return value is 0
+ */
+int main(void)
+{
+	char a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+	int i;
+
+	for (i = 0; a[i] != '\0'; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\n');
+	return (0);
+}
