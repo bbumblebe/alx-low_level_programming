2256f13cb1444e6af9fe81d115ee2cdd85e76273
bbumblebe <musweugeoffrey@gmail.com>
2023-03-19 09:30:30 -0700

    3

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-/** main - This program prints the alphabet
+/** 
+ * main - This program prints the alphabet
  * Return: the return value is 0
  */
-
 int main(void)
 {
-	int letters[26];
+	char letters[26];
 
-	for(int i = 0; i < 26; i++)
+	for (int i = 0; i < 26; i++)
+	{
 		letters[i] = 'a' + i;
-	
-	for(int i =0; i < 26; i++)
+	}
+	for (int i = 0; i < 26; i++)
+	{
 		printf("%c ", letters[i]);
-
+	}
 printf("\n");
+
 return (0);
 }
-
