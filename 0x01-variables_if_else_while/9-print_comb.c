d8212249bb5457ace508b2e4d988efff380e72fc
bbumblebe <musweugeoffrey@gmail.com>
2023-03-19 16:50:30 -0700

    223

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -7,10 +7,15 @@ int main(void)
 {
 	int a[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	int i;
+	int b;
 
 	for (i = 0; a[i] != '\0'; i++)
 	{
+	for (j = 0; a[j] != '\0'; j++)
+	{
 		putchar(a[i]);
+		putchar(a[j]);
+		putchar(' ');
 	}
 	putchar('\n');
 	return (0);
