ea462347351dbe5332968d685860d5f7f9fab37c
bbumblebe <musweugeoffrey@gmail.com>
2023-03-21 15:53:39 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10() - making alphabrt 10 times
+ * Return: void
+ */
+void print_alphabet_x10(void);
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		for (i = 1; i <= 10; i++)
+		{
+			_putchar(i);
+		}
+	}
+_putchar('\n');
+}
+
