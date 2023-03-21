88fa25f5781c41d82df8d0ee4321b9cab7affde2
bbumblebe <musweugeoffrey@gmail.com>
2023-03-21 11:29:10 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ * Return: the return value is 0
+ */
+int main(void)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; a[i] = '\0'; i++)
+	{
+		putchar(a[i]);
+	}
+	putchar('\0');
+	return (0);
+}
