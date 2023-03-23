384e6fc5201c80292a662d54240eb3ba5e7b2ddc
bbumblebe <musweugeoffrey@gmail.com>
2023-03-23 06:14:07 -0700

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _isupper - checks for uppercase character.
+ * Return: 0 or 1
+ * @c: The character to print
+ * On error, -1 is returned.
+ */
+int _isdigit(int c)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		if (c == i)
+		{
+			return (1);
+		}
+	}
+	return (0);
+}
+
