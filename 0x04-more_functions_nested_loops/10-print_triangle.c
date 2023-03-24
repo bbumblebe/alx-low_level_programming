2062b3a556b3832120dc611ddb7033523fad07e4
bbumblebe <musweugeoffrey@gmail.com>
2023-03-24 01:54:57 -0700

    2

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,7 +7,7 @@ void print_triangle(int size)
 {
 	if (size <= 0)
 	{
-		_putchar('n');
+		_putchar('\n');
 	}
 	else
 	{
@@ -25,5 +25,5 @@ void print_triangle(int size)
 			}
 			_putchar('\n');
 		}
-	}
+	};
 }
