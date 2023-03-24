e485b985022a71d57c9411adee65f06d999d8144
bbumblebe <musweugeoffrey@gmail.com>
2023-03-24 02:37:43 -0700

    1

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -18,19 +18,15 @@ int main(void)
 		}
 		else if (i % 5 == 0 && i % 3 != 0)
 		{
-			printf("Buzz");
+			printf(" Buzz ");
 		}
 		else if (i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i == 1)
-		{
-			printf("%d", i);
-		}
 		else
 		{
-			printf(" %d", i);
+			printf(" %d ", i);
 		}
 	}
 	printf("\n");
