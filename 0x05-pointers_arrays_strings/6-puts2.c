d829a475428aaa1fd848786309d20a1cc5e8cff3
bbumblebe <musweugeoffrey@gmail.com>
2023-03-29 12:20:16 -0700

    7

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,10 +10,12 @@ void puts2(char *str)
 
 	for (i = 0; str[i] != '\0'; i++)
 	{
-		if (str[i] != '\0' && str[i]%2 == 0 )
+		if (str[i] != '\0')
 		{
 			_putchar(str[i]);
+		
 		}
+		i++;
 	}
 	_putchar('\n');
 }
