725ec821e1492a213a8b03039a2786e21f4a9b33
bbumblebe <musweugeoffrey@gmail.com>
2023-03-29 11:15:51 -0700

    1

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * puts2 - s function that prints every other char
+ * Return: void
+ */
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] != '\0')
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
