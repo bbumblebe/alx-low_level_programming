a8cd579011df34782c5c6e5f01b516afdd3d265c
bbumblebe <musweugeoffrey@gmail.com>
2023-03-29 09:13:35 -0700

    1

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -10,7 +10,7 @@ void rev_string(char *s)
 	int i, j;
 	char tmp;
 
-	for ( i = 0, j = strlen(s) - 1; i < j; i++, j--)
+	for (i = 0, j = strlen(s) - 1; i < j; i++, j--)
 	{
 		tmp = s[i];
 		s[i] = s[j];
