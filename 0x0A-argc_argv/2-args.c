26d6d49325d527c6c820bc6bd41c299c0a2e1e80
bbumblebe <musweugeoffrey@gmail.com>
2023-04-11 05:53:23 -0700

    8

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -1,7 +1,7 @@
 /**
  * main - the program prints all arguments
  * @argc: the argument c
- * @argz: the pointer
+ * @argv: the pointer
  * Return: 0
  */
 #include <stdio.h>
@@ -9,7 +9,7 @@ int main(int argc, char *argv[])
 {
 	int i;
 
-	for (i = 0; i < argc; i ++)
+	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);
 	}
