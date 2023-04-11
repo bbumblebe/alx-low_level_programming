976d87484050fda5408d9d58b4695b21cd993be8
bbumblebe <musweugeoffrey@gmail.com>
2023-04-11 00:50:41 -0700

    777

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,5 +1,13 @@
+/**
+ * main - we have argc and arg and are ptinting the doc name
+ * @argv: pointer
+ *
+ * @argc: program name and null
+ * Return: our return valuis defined
+ */
+
 #include <stdio.h>
-int  main (int argc, char *argv[])
+int  main(int argc, char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (argc);
