e774a19eea01ccb41ec15b0764abb8b458afb024
bbumblebe <musweugeoffrey@gmail.com>
2023-04-11 05:12:36 -0700

    0

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * times - i decided to use times function to multiply
+ * @arg1: argument 1
+ * @arg2: argument 2
+ * Return: the return value
+ */
+int times(int arg1, int arg2)
+{
+	int a;
+
+	a = arg1 * arg2;
+	return (a);
+}
+/**
+ * main - here we are calulating product
+ * @argc: argument p
+ * @argv: argumen v
+ * Return: we return 0
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+
+	if (argc > 1)
+	{
+	result = times(atoi(argv[1]), atoi(argv[2]));
+	printf("%d\n", result);
+	}
+	if (argc <= 1)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	return (0);
+}
