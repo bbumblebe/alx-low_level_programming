ed902cc656b977f2ecedf4057905732b411dfbd5
bbumblebe <musweugeoffrey@gmail.com>
2023-04-11 03:32:18 -0700

    700

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,6 +1,5 @@
 /**
  * main - the program count args
- * @count: represents number of arguments
  * @argc: argument
  * @argv: pointer argument
  * Return: 0
@@ -8,12 +7,7 @@
 #include <stdio.h>
 int main(int argc, char *argv[])
 {
-	int count;
-
-	for (count = 0; count < argc; count++)
-	{
-		(void) argv;
-			printf("%d\n",argc - 1);
-	}
+	(void) argv;
+	printf("%d\n", argc - 1);
 	return (0);
 }
