4a59e87409c7aa68bbdf98ab905b68e1e0822983
bbumblebe <musweugeoffrey@gmail.com>
2023-04-12 09:57:13 -0700

    55

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,38 @@
+/**
+ * str_concat - string cont program
+ * Return: null or strcon
+ * @strcont: string cont f
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+char *str_concat(char *s1, char *s2)
+{
+	char *strcont;
+	unsigned int i;
+
+	if ((s1 == NULL) || (s2 == NULL))
+	{
+		return (NULL);
+	}
+	else
+	{
+	strcont = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
+	if (strcont == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < strlen(s1); i++)
+	{
+		strcont[i] = s1[i];
+	}
+	for (i = 0; i < strlen(s2); i++)
+	{
+		strcont[i + strlen(s1)] = s2[i];
+	}
+		strcont[strlen(s1) + strlen(s2)] = '\0';
+		return (strcont);
+		free(strcont);
+	}
+}
