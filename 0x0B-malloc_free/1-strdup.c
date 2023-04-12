7f29834d91cd55d86884b41930bdac584eae3962
bbumblebe <musweugeoffrey@gmail.com>
2023-04-12 07:08:18 -0700

    77

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,9 +9,11 @@
 char *_strdup(char *str)
 {
 	int i;
-	char *strdup = malloc(sizeof(char) * (strlen(str)));
 
-	if (strdup == NULL)
+
+	char *strdup = malloc(sizeof(char) * (strlen(str) + 1));
+
+	if (str == NULL)
 	{
 		return (NULL);
 	}
