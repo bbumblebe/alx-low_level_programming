a45cb4799ff63dad19b13f2a0688fe0cc466fc9a
bbumblebe <musweugeoffrey@gmail.com>
2023-04-13 12:55:26 -0700

    0

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void *malloc_checked(unsigned int b)
+{
+	void *checked  = malloc(b);
+	if (checked == NULL)
+	{
+		exit(98);
+	}
+	else
+	{
+		return (checked);
+	}
+}
