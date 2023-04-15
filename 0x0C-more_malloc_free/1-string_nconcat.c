63a209b300f6f21ae90ee2779b199ae696e15242
bbumblebe <musweugeoffrey@gmail.com>
2023-04-15 04:33:38 -0700

    0

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,42 @@
+#include <stdlib.h>
+/**
+ * string_nconcat - str concatation
+ * @s1: string 1
+ * @sw: string 2
+ * @n: number of bytes
+ * Return: point to concat string or NULL
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	char *ncon;
+	unsigned int i, j, len1 = 0, len2 = 0;
+
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	while (s1[len1] != '\0')
+	{
+		len1++;
+	}
+	while (s2[len2] != '\0')
+	{
+		len2++;
+	}
+	if (n >= len2)
+		n = len2;
+	ncon = (char *)malloc((len1 + len2 + 1) * sizeof(char));
+		if (ncon == NULL)
+			return (NULL);
+
+	for (i = 0; i < len1; i++)
+		ncon[i] = s1[i];
+	for (j = 0; s2[j] != '\0' && j < len2; j++)
+		ncon[i + j] = s2[j];
+	ncon[i + j] = '\0';
+	return(ncon);
+}
