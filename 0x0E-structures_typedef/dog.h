9acae469372d3e41be5edf08ff7d135fd17019b6
bbumblebe <musweugeoffrey@gmail.com>
2023-04-17 12:51:50 -0700

    0987

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - a dog data structure
+ * @name: name is here
+ * @age: the age
+ *
+ * @owner: the owner of the dog
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
