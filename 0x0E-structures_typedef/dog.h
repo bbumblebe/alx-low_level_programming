cc6abddd03a120a0c51b57161f3a84d37dc4ecfe
bbumblebe <musweugeoffrey@gmail.com>
2023-04-18 07:11:14 -0700

    002

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,10 @@ struct dog
 	char *owner;
 };
 void init_dog(struct dog *d, char *name, float age, char *owner);
+/**
+ * dog_t - dog structure typedef
+ */
+typedef struct dog dog_t;
 
 void print_dog(struct dog *d);
 #endif
