d16578f9f90f47a64b420166e62c747cac572802
bbumblebe <107593421+bbumblebe@users.noreply.github.com>
2023-04-20 23:17:46 +0200

    I'm really tired

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,2 +1,46 @@
+
+
 #include <stdarg.h>
+
 #include <stddef.h>
+
+/**
+
+ * sum_them_all - Returns the sum of all its parameters.
+
+ * @n: Number of arguments.
+
+ * @...: Variable number of arguments.
+
+ *
+
+ * Return: The sum of all arguments.
+
+ */
+
+int sum_them_all(const unsigned int n, ...)
+
+{
+
+	va_list args;
+
+	unsigned int i;
+
+	int sum = 0;
+
+	if (n == 0)
+
+		return (0);
+
+	va_start(args, n);
+
+	for (i = 0; i < n; i++)
+
+		sum += va_arg(args, int);
+
+	va_end(args);
+
+	return (sum);
+
+}
+
