058f61af138dbcf84bed4ef2283e8829d443f9bd
bbumblebe <musweugeoffrey@gmail.com>
2023-05-02 06:31:49 -0700

    100000000000000000000000000000001

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,12 +1,16 @@
 #include "lists.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * free_listint - list int
+ * @head: head
+ * Return: nothing
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *ptf;
 
-	ptf = head;
-	while (head != NULL)
+	while ((ptf = head) != NULL)
 	{
 		head = head->next;
 		free(ptf);
